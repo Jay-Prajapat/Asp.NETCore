d244649add90cc8ce94dfa99eace5853cc1e0414
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-06-20 20:54:06 +0530

    Completed Practical-16
    Created a sample hello world api and log the message using inbuilt logger.

---
+++ Practical16/Practical16/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practical16.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            logger.LogInformation("Hello world");
+            return Ok("Hello world  successfull");
+        }
+    }
+}
